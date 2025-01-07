8d82ac6c11ce622a81e5516c8fe0e88c6a3dcff6
Milan Shah <21shah.milan@gmail.com>
2025-01-06 23:44:04 -0600

    Added actions enum, added shuffle to reset_deck, added player ids - Fixed Player._alive not having a proper getter - Added a method to get a list of actions available to the player on their turn

--- game_objects.py
+++ game_objects.py
@@ -13,6 +13,18 @@ class Card(Enum):
     CONTESSA = 3
     DUKE = 4
 
+class Actions(Enum):
+    INCOME = "take income"
+    FOREIGN_AID = "take foreign aid"
+    TAX = "collect taxes"
+    STEAL = "steal money"
+    EXCHANGE = "exchange roles"
+    ASSASSINATE = "assassinate an opponent"
+    COUP = "launch a coup"
+    DENY_ASSASSINATION = 'block the assissination'
+    DENY_THEFT = 'block the steal'
+    DENY_AID = 'block the claim to foreign aid'
+
 class Deck:
     """The deck of cards for the running game. Top of the deck is defined as the last index. 
     
@@ -42,10 +54,11 @@ class Deck:
         self.deck.extend([Card.CAPTAIN]*3)
         self.deck.extend([Card.CONTESSA]*3)
         self.deck.extend([Card.DUKE]*3)
+        self.shuffle_cards()
         
 
 class Player:
-
+    #Hidden information
     _active_roles: list[Card] = []
     
     #Public information    
@@ -54,10 +67,19 @@ class Player:
     _bal: int = 0
     _revealed_roles: list[Card] = [] 
     
-    def __init__(self, first_role:Card, second_role:Card):
+    def __init__(self, id:str, first_role:Card, second_role:Card):
+        self._id = id
         self._bal = 2
         self._active_roles = [first_role, second_role]
         self._alive = True
+    
+    @property
+    def id(self) -> str:
+        return self._id
+    
+    @property
+    def alive(self) -> bool:
+        return self._alive
     
     @property
     def bal(self) -> int:
@@ -70,7 +92,7 @@ class Player:
     @property
     def revealed_roles(self) -> list[Card]:
         return self._revealed_roles
-    
+       
     def increment_bal(self, increment:int) -> int:
         '''Increments the balance by the given parameter and returns the final balance.'''
         self._bal += int(increment)
@@ -99,7 +121,17 @@ class Player:
         '''Replaces the role in the provided position with the new role.'''
         assert pos < len(self.active_roles), "Invalid replacement with the current number of active roles."
         self._active_roles[pos] = new_role
-        
+    
+    def get_available_actions(self) -> list[Actions]:
+        base_actions = [Actions.INCOME, Actions.FOREIGN_AID, Actions.TAX, Actions.STEAL, Actions.EXCHANGE]
+        #If more than 10 coins must launch a coup!
+        if self.bal >= 10:
+            return [Actions.COUP]
+        if self.bal > 3: 
+            base_actions.append(Actions.ASSASSINATE)
+        if self.bal > 7:
+            base_actions.append(Actions.COUP)
+        return base_actions
     
     
 
